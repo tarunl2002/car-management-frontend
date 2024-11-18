9487a6397dba554f9bfb9a60ee80624725c1a3a6
Tarun Lalwani <tarunlalwani2002@gmail.com>
2024-11-18 22:17:28 +0530

    api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://car-management-backend-ng2i.onrender.com/api';
 
 
 export const login = async (credentials) => {
